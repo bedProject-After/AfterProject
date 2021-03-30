3a5a8d4516b068f9cfe6ddc13df9647da55dc56a
hzy0711 <email@1633317531@11.com>
2021-03-30 15:03:15 +0800

    '后端第一次提交'

---
+++ src/main/java/org/demo/bedprojectafter/BedprojectAfterApplication.java
@@ -0,0 +1,13 @@
+package org.demo.bedprojectafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedprojectAfterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedprojectAfterApplication.class, args);
+    }
+
+}
